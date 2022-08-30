89d7704a7acba2a70caa932620c9dbb96eca1742
Ram21 <77181225+JCamposStatistics@users.noreply.github.com>
2022-08-29 23:23:13 -0500

    Add files via upload

---
+++ Introduction_to_R.R
@@ -0,0 +1,26 @@
+#Introducción to R
+#Okay okay, let´s to start, so What is R? is a letter of the alphabet oh! Is a little joke of my part. 
+#R is a programming language, but, for what? Basically R is a statistical programming language
+#in this, the probabilistic models, the statistical works, Machine Learning, Deep Learning, R is fundamental tool for that
+#R is an interpreted language (like Java) and not compiled (like C,
+#C++, Fortran, Pascal, . . . ), which means that commands typed on the keyboard are executed
+#directly without the need to build executables.
+#Let´S to begin
+
+#R prompts you with ">", on the console. Imagine. R is like a big calculator, enter your expression, 
+#and R will evaluate the expression and evaluate the result
+#Example:
+1+1
+
+#Remember(For Windows, because is the OS most "common"):
+#Do you wanna exit from R?
+#Select file-> Press save as
+
+#Interrumpting R
+#If R running a long-running and return to the command prompt without exiting R  
+#Either press the Esc key or click on the Stop-sign icon
+
+#Getting help
+#help(functionname)
+#help(package="packagename")
+#RSiteSearch("key phrase")
